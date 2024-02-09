c389d8bfb0834986190b3425824e3356d75ab801
Sudhanshu Bharti <120251980+Sudhanshu-Bharti@users.noreply.github.com>
2024-02-09 20:31:02 +0530

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Toursim",
+  description:
+    "Its Your Life Make it Large",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+
+}
